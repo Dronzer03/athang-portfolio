86c7de855a98915768b54a23f5d5b3c011718077
Athang Kulkarni <athang.kulkarni03@gmail.com>
2021-12-14 19:30:42 +0530

    fix try bullets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import {useEffect} from 'react';
 import HomePage from "./Components/Home/HomePage";
 import About from "./Components/About/About";
 import SideIcons from "./Components/SideIcons";
@@ -7,6 +8,10 @@ import Education from "./Components/Education/Education";
 import Contact from "./Components/Contact/Contact";
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Athang"
+  }, [])
 
   return (
     <div className="App">
